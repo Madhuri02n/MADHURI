9c606af0bb6789033341489a9f5bf9b8f976a75b
Madhuri02n <139317419+Madhuri02n@users.noreply.github.com>
2024-12-28 00:57:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+open cmd
+run the following to create a folder
+>mkdir hello-selenium
+>cd hello-selenium
+
+initialize node project
+>npm init -y
+
+install selenium-webdriver package
+>npm install selenium-webdriver
+
+create index.js file
+add the following code in index.js
+
+
+
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
 
